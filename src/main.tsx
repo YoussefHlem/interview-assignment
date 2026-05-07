c0e7e80a3cd3a61f1a74409718b300814aa1ac41
YoussefHlem <youssefhlemdev@gmail.com>
2026-05-07 05:51:35 +0300

    feat(query): add QueryProvider component for React Query integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryProvider } from "./lib/query-client.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryProvider>
+      <App />
+    </QueryProvider>
   </StrictMode>,
-)
+);
